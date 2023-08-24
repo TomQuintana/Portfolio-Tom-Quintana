393b5e63161fd32f7d41042ff3c2b4417f5f09df
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-24 19:28:26 -0300

    add botton for aboutMe

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import '../styles/aboutMe.css';
 
 const AboutMe = () => {
@@ -13,8 +14,13 @@ const AboutMe = () => {
             <p>Soy un apasionando del desarrollo de software que esta en constante aprendizaje sobre nuevas tecnologias y como generar codigo mas limpio. Me gusta generar proyectos para solucionar problemas que me surgen en la cotidianidad y poder aprender cada vez mas. Cuando no me encuentro codeando suelo ver documentales, leer o salir a caminar. 
             </p>
           </div>
-          <div>
-            Aca expando un poco mas de mi { '->' } 
+          <div className='mt-4 mb-4'>
+            Aca expando un poco mas de mi { '-> ' } 
+            <Link href={'/aboutMe'}
+              className='bg-orange-400 p-1 text-white rounded-2xl'
+            >
+              Sobre mi 
+            </Link>
           </div>
         </div>
       </div>
