fc5a924222983a5c3e1c6a04a00e69e0e6c88374
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-24 19:31:26 -0300

    add aboutMe page and button

--- src/app/pageFormat/Nav.page.tsx
+++ src/app/pageFormat/Nav.page.tsx
@@ -17,7 +17,12 @@ const data = [
     name : 'Curriculum',
     img: 'https://img.icons8.com/ios/50/000000/resume.png',
     path: 'https://drive.google.com/file/d/1mnyEk4dlkoG3y9wKQHqi1a6qWupj2dw1/view'
-  }
+  },
+  {
+    name : 'Sobre Mi',
+    img: 'https://img.icons8.com/external-prettycons-lineal-prettycons/49/000000/external-portfolio-user-interface-vol-3-prettycons-lineal-prettycons-4.png',
+    path: '/proyectos'
+  },
 ];
 
 const author = {
