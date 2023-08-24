00e958fb205c38d0c492ed450dfaea4bd8e2cdfe
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-24 19:16:57 -0300

    change font and add css

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -4,16 +4,17 @@ import '../styles/aboutMe.css';
 const AboutMe = () => {
   return (
     <> 
-      <div className='flex justify-center text-justify aboutMeQuestion'>
+      <div className='flex justify-center text-justify aboutMeQuestion font-serif'>
         <div className='grid grid-cols-1 '>
-          <div className='mt-12 flex justify-start'>
-            <h1 className='underline text-2xl'>Sobre Mi</h1>
+          <div className='mt-8 flex justify-start font-mono'>
+            <h1 className='text-2xl textUnderline'>Sobre Mi</h1>
           </div>
-          <div className='flex justify-center'>
-            <p>Soy un desarrollador de Software Backend con sede en Argentina. Actualmente tengo experiencia desarrollando con Node y Typescript pero continuo en constante aprendizaje. Me encanta desarrollar porque me otorga las herramientas
-              para poder generar soluciones a problemas cotidianos como por ejemplo PROYECTO MEALS y poder llevar a cabo mis ideas como por ejemplo PROYECTO POMODORO . Cuando no estoy desarrollando suelo salir a caminar,
-              leer o ver documentales. 
+          <div className='flex justify-center p-1'>
+            <p>Soy un apasionando del desarrollo de software que esta en constante aprendizaje sobre nuevas tecnologias y como generar codigo mas limpio. Me gusta generar proyectos para solucionar problemas que me surgen en la cotidianidad y poder aprender cada vez mas. Cuando no me encuentro codeando suelo ver documentales, leer o salir a caminar. 
             </p>
+          </div>
+          <div>
+            Aca expando un poco mas de mi { '->' } 
           </div>
         </div>
       </div>
