a1f12c926279324bf83319d85b28e4a8d196fafb
tomasquintana-gj <tomquintana20@gmail.com>
2023-09-04 15:17:27 -0300

    test font size

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <div className='flex justify-center text-justify aboutMeQuestion '>
         <div className='grid grid-cols-1 '>
           <div className='mt-8 flex justify-start font-mono'>
-            <h1 className='text-xl textUnderline'>Sobre Mi</h1>
+            <h1 className='text-xl textUnderline logotipoFont'>Sobre Mi</h1>
           </div>
           <div className='flex justify-center p-1'>
             <p>Soy un apasionado del desarrollo de software, por lo que estoy en constante aprendizaje sobre nuevas tecnologías y sobre cómo generar código más limpio. Me gusta generar proyectos para solucionar problemas que surgen en la cotidianidad y poder aprender cada vez más. Cuando no me encuentro codeando suelo ver documentales, leer o salir a caminar.
